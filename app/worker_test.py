c8fd1fb90adb80d38c41c43ac8eb585353df9827
Basil Dubyk <samitnuk.work@gmail.com>
2019-10-08 18:09:03 +0300

    Add memory and disk usage info to error message in `worker_test.py`

--- app/worker_test.py
+++ app/worker_test.py
@@ -33,10 +33,11 @@ def check_celery_workers_is_running():
     Notifies through Slack when there are no active Celery workers.
     """
     if not is_process_running('celery'):
-        error_message = '*Error:* Celery workers are not running.'
-        send_slack_message(text=error_message)
-    else:
-        error_message = '*Error:* Celery workers are not running.'
+        memory_info = psutil.virtual_memory()
+        disk_info = psutil.disk_usage('/')
+        error_message = (
+            '*Error:* Celery workers are not running.\nMemory used: *{}%*\nDisk used: *{}%*'
+        ).format(memory_info.percent, disk_info.percent)
         send_slack_message(text=error_message)
 
 
